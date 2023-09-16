787af18389812461811f841c913b2d02bc55342b
Amoah <amoahtnt6@gmail.com>
2023-09-16 08:15:06 +0000

    Added a data validation

--- src/middleware/data_validation.ts
+++ src/middleware/data_validation.ts
@@ -1,24 +1,24 @@
-import validator from "express-validator";
+import validator, { ValidationChain } from "express-validator";
 
-export const userValidation = {
-  signUpValidation: [
-    validator
-      .body("username")
-      .notEmpty()
-      .withMessage("Username is required")
-      .isLength({ min: 2 })
-      .withMessage("Username must be at least 2 characters"),
-    validator
-      .body("email")
-      .notEmpty()
-      .withMessage("Email is required")
-      .isEmail()
-      .withMessage("Email is invalid"),
-    validator
-      .body("password")
-      .notEmpty()
-      .withMessage("Password is required")
-      .isLength({ min: 6 })
-      .withMessage("Password must be at least 6 characters"),
-  ],
-};
+const signUpValidation: Array<ValidationChain> = [
+  validator
+    .body("username")
+    .notEmpty()
+    .withMessage("Username is required")
+    .isLength({ min: 2 })
+    .withMessage("Username must be at least 2 characters"),
+  validator
+    .body("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Email is invalid"),
+  validator
+    .body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters"),
+];
+
+export { signUpValidation };
--- src/routers/users_router.ts
+++ src/routers/users_router.ts
@@ -1,11 +1,11 @@
 import { getAllUsers,createUser } from "../controller/user_controller";
-import { userValidation} from "../middleware/data_validation";
+import { signUpValidation} from "../middleware/data_validation";
 import { errorValidation} from "../middleware/error_validation";
 import { Router } from "express";
 
 const router = Router();
 
 router.get("/", getAllUsers);
-router.route("/create").post(userValidation.signUpValidation, errorValidation, createUser);
+router.route("/create").post(signUpValidation, errorValidation, createUser);
 
 export default router;
